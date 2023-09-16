f57ec373e6b39cdb67b809a03b448b0ce561b0b5
kkkovalchukkk <student.kovalchuk@yandex.ru>
2023-09-16 22:29:10 +0300

    1024px 80%

---
+++ scripts/main-accordeon.js
@@ -0,0 +1,14 @@
+const accordeonsEls = document.querySelectorAll('.accordeon');
+
+accordeonsEls.forEach((acc) => {
+  acc.addEventListener('click', () => {
+    const content = acc.querySelector('.accordeon__content');
+    if (acc.classList.contains('accordeon--active')) {
+      acc.classList.remove('accordeon--active');
+      content.style.maxHeight = '0';
+    } else {
+      acc.classList.add('accordeon--active');
+      content.style.maxHeight = content.scrollHeight + 'px';
+    }
+  });
+});
--- scripts/main-swiper.js
+++ scripts/main-swiper.js
@@ -1,8 +1,30 @@
 const winSectionSwiper = new Swiper('.win-section__swiper', {
   direction: 'horizontal',
-  slidesPerView: 3,
+  slidesPerView: 'auto',
   loop: true,
   spaceBetween: 20,
+  breakpoints: {
+    320: {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+    },
+    568: {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+    },
+    1280: {
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+    },
+  },
   navigation: {
     nextEl: '.win-section__swiper-navigation-right',
     prevEl: '.win-section__swiper-navigation-left',
@@ -10,7 +32,7 @@ const winSectionSwiper = new Swiper('.win-section__swiper', {
 });
 const reviewSectionSwiper = new Swiper('.reviews-section__swiper', {
   direction: 'horizontal',
-  slidesPerView: 3,
+  slidesPerView: 'auto',
   loop: true,
   spaceBetween: 20,
   navigation: {
@@ -20,7 +42,7 @@ const reviewSectionSwiper = new Swiper('.reviews-section__swiper', {
 });
 const teamSectionSwiper = new Swiper('.team-section__swiper', {
   direction: 'horizontal',
-  slidesPerView: 4,
+  slidesPerView: 'auto',
   loop: true,
   spaceBetween: 20,
   navigation: {
@@ -30,7 +52,7 @@ const teamSectionSwiper = new Swiper('.team-section__swiper', {
 });
 const materialsSectionSwiper = new Swiper('.materials-section__swiper', {
   direction: 'horizontal',
-  slidesPerView: 4,
+  slidesPerView: 'auto',
   loop: true,
   spaceBetween: 20,
   navigation: {
