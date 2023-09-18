8bfa0bad0ad6249be9144cd7390a283adf2d77b0
kkkovalchukkk <student.kovalchuk@yandex.ru>
2023-09-18 22:46:15 +0300

    added validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -2,23 +2,78 @@ window.addEventListener('DOMContentLoaded', () => {
   const forms = document.querySelectorAll('.form');
   const telInputs = document.querySelectorAll('input[name="tel"]');
   const nameInputs = document.querySelectorAll('input[name="name"]');
+  const checkboxInputs = document.querySelectorAll('input[name="politics"');
 
   const submitForm = (e) => {
     e.preventDefault();
 
     const nameInput = e.target.name;
     const telInput = e.target.tel;
+    const checkBoxInput = e.target.politics;
+
+    if (!checkBoxInput.checked) {
+      checkBoxInput.classList.add('invalid');
+    }
+
+    [nameInput, telInput].forEach((input) => {
+      if (!input.value) {
+        input.classList.add('invalid');
+      } else {
+        input.classList.remove('invalid');
+      }
+    });
+
+    if (
+      [nameInput, telInput, checkBoxInput].some((input) =>
+        input.classList.contains('invalid')
+      )
+    ) {
+      return;
+    } else {
+      e.target.submit();
+    }
   };
 
-  nameInputs.forEach((nameInput) => {
-    nameInput.addEventListener('input', (e) => {
-      const inputValue = e.target.value;
+  nameInputs.forEach((input) => {
+    input.addEventListener('input', (event) => {
+      if (event.target.value.length > 15) {
+        event.target.value = event.target.value.slice(0, 15);
+      }
+      const inputValue = event.target.value;
+      const regex = /^[a-zA-Zа-яА-Я]+$/;
+
+      if (!regex.test(inputValue)) {
+        event.target.value = inputValue.replace(/[^a-zA-Zа-яА-Я]+/g, '');
+      }
+      if (event.target.value.length < 2 || event.target.value.length > 30) {
+        input.classList.add('invalid');
+      } else {
+        input.classList.remove('invalid');
+      }
     });
   });
 
   telInputs.forEach((tel) => {
     IMask(tel, {
       mask: '+{7} (000) 000-00-00',
+    });
+
+    tel.addEventListener('input', (e) => {
+      if (!e.target.value || e.target.value.length != 18) {
+        tel.classList.add('invalid');
+      } else {
+        tel.classList.remove('invalid');
+      }
+    });
+  });
+
+  checkboxInputs.forEach((check) => {
+    check.addEventListener('input', () => {
+      if (check.checked) {
+        check.classList.remove('invalid');
+      } else {
+        check.classList.add('invalid');
+      }
     });
   });
 
